644b178c68bcdd590d5dcb66f02b6d20722dd807
Matthew Capdeville <skraz@protonmail.com>
2019-10-01 00:43:20 -0500

    google login stuff

--- public/login.js
+++ public/login.js
@@ -3,5 +3,16 @@ function onSignIn(googleUser) {
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'localhost:3000');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onload = function() {
+    console.log('Signed in as: ' + xhr.responseText);
+  };
+  xhr.send('idtoken=' + id_token);
+  xhr.send('profilePicture=' + profile.getImageUrl());
 }
--- server.js
+++ server.js
@@ -8,9 +8,25 @@ app.use(express.static('public'));
 
 function onConnection(socket){
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
-  socket.on('clear', clearCanvas) => socket.broadcast('clear', clearCanvas);
+  // socket.on('clear', clearCanvas) => socket.broadcast('clear', clearCanvas);
 }
 
 io.on('connection', onConnection);
 
 http.listen(port, () => console.log('listening on port ' + port));
+
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+}
+verify().catch(console.error);
