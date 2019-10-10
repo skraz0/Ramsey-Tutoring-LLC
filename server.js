f72d5672326f4620a6c4717c491b03d57a16cac8
Gerard <gerard.frommeyer@selu.edu>
2019-10-10 16:02:31 -0500

    chat feature on the board wow.
    Co-Authored-By: null <skraz0@users.noreply.github.com>

--- server.js
+++ server.js
@@ -8,18 +8,13 @@ app.use(express.static('public'));
 
 function onConnection(socket){
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
-<<<<<<< HEAD
   // socket.on('clear', clearCanvas) => socket.broadcast('clear', clearCanvas);
-=======
-//  socket.on('clear', clearCanvas) => socket.broadcast('clear', clearCanvas);
->>>>>>> d5f63738ebfbcb80f7c4615bd6f8232b1d3f5f14
 }
 
 io.on('connection', onConnection);
 
 http.listen(port, () => console.log('listening on port ' + port));
 
-<<<<<<< HEAD
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/chattest.html');
 });
@@ -41,20 +36,3 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-=======
-const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client(CLIENT_ID);
-async function verify() {
-  const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
-  const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  // If request specified a G Suite domain:
-  //const domain = payload['hd'];
-}
-verify().catch(console.error);
->>>>>>> 7aa4c3029670fbc5c82a6323f0c47087292c227b
