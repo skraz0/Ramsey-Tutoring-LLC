fd4c7820b787c28938cc4b61bccce9abb20a5d11
Matthew Capdeville <xertot019@gmail.com>
2019-10-13 15:49:49 -0500

    added endpoints for each page

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+// endpoints for all pages of the site
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/terms', function (req, res) {
+  res.sendFile(__dirname + '/public/ToS.html');
+});
+app.get('/scheduler', function (req, res) {
+  res.sendFile(__dirname + '/public/scheduler.html');
+});
+app.get('/session', function (req, res) {
+  res.sendFile(__dirname + '/public/board.html');
+});
+
+// start of socket.io stuff
 function onConnection(socket){
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
   // socket.on('clear', clearCanvas) => socket.broadcast('clear', clearCanvas);
@@ -15,20 +30,16 @@ io.on('connection', onConnection);
 
 http.listen(port, () => console.log('listening on port ' + port));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/chattest.html');
-});
-
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log(socket.id + ' has connected');
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log(socket.id + ' has disconnected');
   });
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    console.log(socket.id + ': ' + msg);
   });
 });
 io.on('connection', function(socket){
