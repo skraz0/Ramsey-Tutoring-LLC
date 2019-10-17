9db23480c72de0c3afd9fa02eabb4be16a60416b
Matthew Capdeville <skraz@protonmail.com>
2019-10-17 16:55:41 -0500

    stuff

--- public/login.js
+++ public/login.js
@@ -3,16 +3,27 @@ function onSignIn(googleUser) {
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Logged in as ' + profile.getName());
+
+  $(".data").css("display","block");
+  $("#pic").attr('src',profile.getImageUrl());
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    window.location.href = 'https://ramsey-tutoring-llc.herokuapp.com/terms';
+  });
+}
+
+  function onFailure(error) {
+    console.log(error);
+  }
 
   var id_token = googleUser.getAuthResponse().id_token;
   console.log("ID Token: " + id_token);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'localhost:3000');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function() {
-    console.log('Signed in as: ' + xhr.responseText);
-  };
-  xhr.send('idtoken=' + id_token);
-  xhr.send('profilePicture=' + profile.getImageUrl());
-}
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+      location.reload();
+    });
+  }
