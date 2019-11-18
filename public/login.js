e05f43c230449992138ee1a88f6663b9b6ab7006
Matthew Capdeville <skraz@protonmail.com>
2019-11-18 14:50:04 -0600

    more progress

--- public/login.js
+++ public/login.js
@@ -1,31 +1,14 @@
-// function onSignIn(googleUser) {
-//   var profile = googleUser.getBasicProfile();
-//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-//   console.log('Name: ' + profile.getName());
-//   console.log('Image URL: ' + profile.getImageUrl());
-//   console.log('Logged in as ' + profile.getName());
-//
-//   $(".data").css("display","block");
-//   $("#pic").attr('src',profile.getImageUrl());
-//   $('#signOutButton').css("display","block");
-//   var auth2 = gapi.auth2.getAuthInstance();
-//   // auth2.signOut().then(function () {
-//   //   window.location.href = '/terms';
-//   // });
-//   window.location.href = '/terms';
-// }
-//
-//   function onFailure(error) {
-//     console.log(error);
-//   }
-//
-//   var id_token = googleUser.getAuthResponse().id_token;
-//   console.log("ID Token: " + id_token);
-//
-//   function signOut() {
-//     var auth2 = gapi.auth2.getAuthInstance();
-//     auth2.signOut().then(function () {
-//       console.log('User signed out.');
-//       location.reload();
-//     });
-//   }
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Email: ' + profile.getEmail());
+};
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+    location.reload();
+  });
+};
